5fac957de75c8a2032f837b984e25d8b21be3121
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-09 16:21:38 -0700

    add: date (functions)

--- src/js/date.js
+++ src/js/date.js
@@ -1,10 +1,32 @@
-export function printDate() {
+export function getYearMonthDay() {
   const date = new Date();
-
   const year = date.getUTCFullYear();
   const month = date.getUTCMonth() + 1;
   const day = date.getUTCDate();
+  return `${year}/${month}/${day}`;
+}
 
-  const fullDate = `${year}/${month}/${day}`;
-  console.log(fullDate);
+export function getMonthDay() {
+  const date = new Date();
+  const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
+  return `${month}/${day}`;
+}
+
+export function getYear() {
+  const date = new Date();
+  const year = date.getUTCFullYear();
+  return `${year}`;
+}
+
+export function getMonth() {
+  const date = new Date();
+  const month = date.getUTCMonth() + 1;
+  return `${month}`;
+}
+
+export function getDay() {
+  const date = new Date();
+  const day = date.getUTCDate();
+  return `${day}`;
 }
