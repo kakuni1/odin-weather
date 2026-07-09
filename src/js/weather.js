97d870db7f72bc9d69cdc2c36b379210aa97feeb
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-08 20:52:32 -0700

    add: weather (basic print) with error logging

--- src/js/weather.js
+++ src/js/weather.js
@@ -6,9 +6,19 @@ const KEY = import.meta.env.VITE_VISUALCROSSING_KEY;
 export async function fetchWeather() {
   try {
     const response = await fetch(weatherUrl(CITY, KEY));
+    if (!response.ok)
+      throw new Error(`http ${response.status} ${response.statusText}`);
     const data = await response.json();
+    printWeather(data);
     console.log(data);
   } catch (error) {
-    console.log(`error: ${error}`);
+    console.error(error);
   }
+}
+
+function printWeather(data) {
+  const div = document.querySelector(".weather");
+  const day = document.createElement("div");
+  day.textContent = `temp: ${data.days[0].temp}`;
+  div.appendChild(day);
 }
