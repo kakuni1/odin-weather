33c455a314b2f1c767f011ace9c051ef2d2311aa
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-10 22:55:24 -0700

    update: getWeather weather.js now handles data only

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,21 @@
 import { Parasol } from "lucide";
 import { setFavicon } from "./icons.js";
-import { fetchWeather } from "./weather.js";
+import { fetchWeather, getWeather } from "./weather.js";
 
-const key = import.meta.env.VITE_VISUALCROSSING_KEY;
 const city = "tokyo";
+const key = import.meta.env.VITE_VISUALCROSSING_KEY;
 
-setFavicon(Parasol, "#fafafa");
-fetchWeather(city, key);
+async function main() {
+  // generate favicon from icon set & set color
+  setFavicon(Parasol, "#fafafa");
+  // uses api to receive weather data
+  // returns as an object
+  const weatherData = await fetchWeather(city, key);
+  console.log(weatherData);
+
+  // set # of days (weather data)
+  const temp = getWeather(weatherData, 3);
+  console.log(temp);
+}
+
+main();
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,5 +1,11 @@
 import { weatherUrl } from "./api.js";
-import { getWeatherIcon } from "./icons.js";
+
+export function getWeather(data, days) {
+  return data.days.slice(0, days).map((day) => ({
+    temp: Math.round(day.temp),
+    icon: day.icon,
+  }));
+}
 
 export async function fetchWeather(city, key) {
   try {
@@ -7,26 +13,8 @@ export async function fetchWeather(city, key) {
     if (!response.ok)
       throw new Error(`http:${response.status} ${response.statusText}`);
     const data = await response.json();
-    printWeatherCards(data);
-    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
-  }
-}
-
-function printWeatherCards(data) {
-  const weather = document.querySelector("#weather");
-  for (const day of data.days.slice(0, 5)) {
-    const dayElement = document.createElement("div");
-    dayElement.classList.add("weather-card");
-
-    const dayTemperature = document.createElement("p");
-    const maxTemperature = Math.round(day.tempmax);
-    dayTemperature.textContent = `max: ${maxTemperature}°C`;
-    const icon = getWeatherIcon(day.icon);
-    icon.classList.add("weather-icon");
-
-    dayElement.append(dayTemperature, icon);
-    weather.appendChild(dayElement);
   }
 }
