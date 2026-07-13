c4e246d4dd37c02b382ee0f25291ca9eccbfe294
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-12 17:10:32 -0700

    remove: unused

--- src/js/date.js
+++ src/js/date.js
@@ -1,36 +1,3 @@
-export function getYearMonthDay() {
-  const date = new Date();
-  const year = String(date.getUTCFullYear());
-  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-  const day = String(date.getUTCDate()).padStart(2, "0");
-  return `${year}/${month}/${day}`;
-}
-
-export function getMonthDay() {
-  const date = new Date();
-  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-  const day = String(date.getUTCDate()).padStart(2, "0");
-  return `${month}/${day}`;
-}
-
-export function getYear() {
-  const date = new Date();
-  const year = String(date.getUTCFullYear());
-  return `${year}`;
-}
-
-export function getMonth() {
-  const date = new Date();
-  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-  return `${month}`;
-}
-
-export function getDay() {
-  const date = new Date();
-  const day = String(date.getUTCDate()).padStart(2, "0");
-  return `${day}`;
-}
-
 export function getCityTime(timezone) {
   return new Date().toLocaleTimeString("en-US", {
     hour: "2-digit",
