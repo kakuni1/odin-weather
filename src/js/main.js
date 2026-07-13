ea0709560f22cbb886c839c319fd99f51f2c9820
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-12 17:07:58 -0700

    add: searchbox event listener & seperate functions, init & setup

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,54 @@ import { setFavicon } from "./icons.js";
 import { printMainCard } from "./print.js";
 import { fetchWeather, getWeatherCurrent } from "./weather.js";
 
-const city = "tokyo";
+// generate favicon from icon set & set color
+setFavicon(Parasol, "#fafafa");
+
 const key = import.meta.env.VITE_VISUALCROSSING_KEY;
 
-async function main() {
-  // generate favicon from icon set & set color
-  setFavicon(Parasol, "#fafafa");
-  // uses api to receive weather data
-  // returns as an object
+async function setup() {
+  const submit = document.querySelector("#form");
+  submit.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#city-input");
+    const city = input.value.trim();
+    if (!city) return;
+
+    // uses api to receive weather data
+    // returns as an object
+    const weatherData = await fetchWeather(city, key);
+    if (!weatherData) {
+      input.classList.add("error");
+      return;
+    }
+
+    input.classList.remove("error");
+    console.log(weatherData);
+
+    // get today's current weather (temp & icon)
+    const currentData = getWeatherCurrent(weatherData);
+    console.log(currentData);
+
+    // print today's current weather onto page
+    // print card if no card, if card delete then print
+    const exists = document.querySelector("#main-card");
+    if (exists) exists.remove();
+    printMainCard(currentData);
+  });
+}
+
+async function init() {
+  // initialize default state
+  const city = "tokyo";
+
   const weatherData = await fetchWeather(city, key);
   console.log(weatherData);
 
-  // get today's current weather (temp & icon)
   const currentData = getWeatherCurrent(weatherData);
   console.log(currentData);
 
   printMainCard(currentData);
 }
 
-main();
+init();
+setup();
